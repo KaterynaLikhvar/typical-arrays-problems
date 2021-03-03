5025d56bb7d9a30d1e5109255b3e36e24854d619
Kate <likhvar.ks@gmail.com>
2021-03-03 15:25:40 +0200

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+    
+  let minimum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < minimum) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  let maximum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] > maximum) {
+      maximum = array[i];
+    }
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  let average = 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    average = sum / array.length;
+  }
+  return average;
 }
